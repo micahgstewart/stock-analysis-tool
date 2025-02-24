9ff34c6a71a106ba8ff70ab7312d1c9e592e4f9a
micahgstewart <micahgstewart@gmail.com>
2025-02-23 18:02:38 -0600

    #comment Changed server name

--- config/config.py
+++ config/config.py
@@ -2,6 +2,9 @@ class Config:
     # Flask configuration
     SECRET_KEY = 'your-secret-key-here'
     
+    # Server settings
+    SERVER_NAME = 'stock-analysis-tool.local:8050'  # Custom domain name
+    
     # Additional configuration settings
     DEBUG = True
     TESTING = False 
