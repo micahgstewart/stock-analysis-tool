6e5140111629f1e6ae149242f0aad9a7adde906d
micahgstewart <micahgstewart@gmail.com>
2025-02-23 18:04:56 -0600

    #comment messing around

--- config/config.py
+++ config/config.py
@@ -2,9 +2,6 @@ class Config:
     # Flask configuration
     SECRET_KEY = 'your-secret-key-here'
     
-    # Server settings
-    SERVER_NAME = 'stock-analysis-tool.local:8050'  # Custom domain name
-    
     # Additional configuration settings
     DEBUG = True
     TESTING = False 
--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run_server(debug=True) 
+    app.run_server(
+        debug=True,
+        host='127.0.0.1',  # Local host
+        port=8050,         # Port number
+    ) 
